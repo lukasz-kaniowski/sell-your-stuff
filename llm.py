ae23c8a8dccc296ec73ed43d21376fcbf5cd8bec
Lukasz Kaniowski <lukasz.kaniowski@gmail.com>
2024-05-13 14:43:56 +0100

    use anthropic client

--- llm.py
+++ llm.py
@@ -1,8 +1,7 @@
 import base64
-from langchain_anthropic import ChatAnthropic
-from langchain_core.messages import HumanMessage
+import anthropic
 
-chat = ChatAnthropic(model="claude-3-haiku-20240307")
+client = anthropic.Anthropic()
 
 prompt = """Describe the image as if you were selling it on an online marketplace.
 Include the item's condition, size, color, and any other relevant details.
@@ -12,23 +11,30 @@ Title should contain the brand name.
 
 
 def get_item_description(image):
-    # img1_base64 = base64.b64encode(img1_path.read_bytes()).decode("utf-8")
     img1_base64 = base64.b64encode(image.read()).decode("utf-8")
-    print(image)
-    messages = [
-        HumanMessage(
-            content=[
-                {
-                    "type": "image_url",
-                    "image_url": {
-                        "url": f"data:{image.type};base64,{img1_base64}",
+    message = client.messages.create(
+        model="claude-3-haiku-20240307",
+        max_tokens=1272,
+        temperature=0,
+        messages=[
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "image",
+                        "source": {
+                            "type": "base64",
+                            "media_type": image.type,
+                            "data": img1_base64
+                        }
                     },
-                },
-                {
-                    "type": "text",
-                    "text": prompt
-                },
-            ]
-        )
-    ]
-    return chat.invoke(messages).content
+                    {
+                        "type": "text",
+                        "text": prompt
+                    }
+                ]
+            }
+        ]
+    )
+
+    return message.content[0].text
